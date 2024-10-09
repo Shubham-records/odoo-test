425b5c6459709d7322d0f3c884757c01c9672456
Shubham-records <153800054+Shubham-records@users.noreply.github.com>
2024-10-09 23:41:22 +0530

    commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "InheritModuleTestShub",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "My Company",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['hr'],
+
+    'data': [
+        'views/views.xml',
+    ],
+    
+}
---
+++ models/custom_models.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+class inherit_module_test_shub(models.Model):
+
+    _inherit = 'hr.employee'
+
+
